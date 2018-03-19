7f48d2e92eb3d80930873aba7b516535d7b4fa1b
Erwann de Dieuleveult <ededieuleveult@gmail.com>
2018-03-19 11:52:24 +0100

    add validations on models

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,9 @@
 class Book < ApplicationRecord
+    belongs_to :user
+
+    attr_accessor :title, :author, :resume, :state
+
+    validates :title, presence: true, uniqueness: true
+    validates :author, presence: true
+    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    has_many :books
+
+    attr_accessor :first_name, :email, :last_name
+
+    validates :first_name, last_name, presence: true
+    validates :email, presence: true, uniqueness: true
+    validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 end
