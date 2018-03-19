fb6beaa7964749e06a70cf89e215f3c19880151b
Erwann de Dieuleveult <ededieuleveult@gmail.com>
2018-03-19 15:28:09 +0100

    add resources for users and books

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :books
-
-  get "users", to: "users#index"
-  
-  get "users/:id", to: "users#show", as: :user
-
-  delete "users/:id", to: "users#destroy"
+  resources :users
 end
