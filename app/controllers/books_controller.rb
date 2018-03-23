afeecd6953c0f74b6960347c026ae5bb4e40ff2c
Erwann de Dieuleveult <ededieuleveult@gmail.com>
2018-03-23 14:37:11 +0100

    fix CRUD for book and user

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,14 +2,32 @@ class BooksController < ApplicationController
     before_action :set_book, only: [:show, :edit, :update, :destroy]
 
     def index
-        @books = Book.all
+        @books = Book.all.order('title')
     end
 
     def show
-        @book
+    end
+
+    def new
+        @book = Book.new
+    end
+
+    def create
+        @book = Book.new(book_params)
+       
+        if @book.save
+            redirect_to @book
+        else
+            render 'new'
+        end
     end
 
     def edit
+    end
+
+    def update
+        @book.update(book_params)
+        redirect_to book_path
     end
 
     def borrowed
@@ -26,6 +44,10 @@ class BooksController < ApplicationController
 
     def set_book
         @book = Book.find(params[:id])
+    end
+
+    def book_params
+        params.require(:book).permit(:title, :author)
     end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,32 @@
 class UsersController < ApplicationController
-    before_action :set_user, only: [:show, :destroy, :update]
+    before_action :set_user, only: [:show, :edit, :destroy, :update]
 
     def index
-        @users = User.all
+        @users = User.all.order('last_name')
     end
 
     def show
-        @user
+    end
+
+    def new
+        @user = User.new
     end
 
     def create
         @user = User.new(user_params)
-        @user.save
-        redirect_to user_path(@user)
+        if @user.save
+            redirect_to @user
+        else
+            render 'new'
+        end
+    end
+
+    def edit        
     end
 
     def update
         @user.update(user_params)
+        redirect_to user_path
     end
 
     def destroy
