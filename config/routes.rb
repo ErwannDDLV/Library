3a82107c6a72fc230b8cd3ec7641766c9dd19498
Erwann de Dieuleveult <ededieuleveult@gmail.com>
2018-03-30 14:59:24 +0200

    fix routes for reservations

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-  resources :books
   resources :users
-
-  get "users", to: "users#index"
-
-  get "users/new", to: "users#new"
-  post "users", to: "users#create"
-
-  # NB: The `show` route needs to be *after* `new` route.
-  get "users/:id", to: "users#show"
-
-  get "users/:id/edit", to: "users#edit"
-  patch "users/:id", to: "users#update"
-
-  delete "users/:id", to: "users#destroy"
-
-
-
+  resources :books
+  resources :bookings
 
 end
