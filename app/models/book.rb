2712709fefa1a7ddeda2519c8d70f1c1b661ce78
Erwann de Dieuleveult <ededieuleveult@gmail.com>
2018-03-30 15:01:59 +0200

    add aasm in Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,5 +7,17 @@ class Book < ApplicationRecord
     has_attached_file :book_cover, styles: { book_index: "100x200>", book_show: "325x475>" }, default_url: "/assets/:style/bookcover.jpg"
     validates_attachment_content_type :book_cover, content_type: /\Aimage\/.*\z/
 
-    scope :state, -> { where(state: true) }
+    include AASM
+    aasm do
+        state :available, initial: true
+        state :borrowed
+
+        event :borrow do
+            transitions from: :available, to: :borrowed
+        end
+
+        event :return do
+            transitions from: :borrowed, to: :available
+        end
+    end
 end
